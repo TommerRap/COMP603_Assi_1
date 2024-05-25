010ca7c90c42ff8a30b7ba36d1a703f577aeaf0a
YJason <70372800+platinumwolaopo@users.noreply.github.com>
2024-05-25 21:15:37 +1200

    electric vehicle

--- src/Models/ElectricVehicle.java
+++ src/Models/ElectricVehicle.java
@@ -6,8 +6,51 @@ package Models;
 
 /**
  *
- * @author y2046
+ * @author Qijia Yuan 22176238
  */
-public class ElectricVehicle {
+public class ElectricVehicle extends Vehicle {
+    
+    private int endurance;   
+    private boolean superCharge;
+    private int superChargeRate;
+   
+    
+    public ElectricVehicle(int endurance, boolean superCharge, int superChargeRate, int id, String make, String model, int year, String licence, double price, boolean isSold )
+    {
+        super(id, make, model, year, licence, price, isSold);
+         this.endurance=endurance;
+         this.superCharge=superCharge;
+         this.superChargeRate=superChargeRate;         
+    }
+ 
+    public int getEndurance()
+    {
+        return endurance;
+    }
+    
+    public boolean isSuperCharge()
+    {
+        return superCharge;
+    }
+    
+    public int getSuperChargeRate()
+    {
+        return superChargeRate;
+    }
+    
+    public void setEndurance(int endurance)
+    {
+        this.endurance=endurance;
+        
+    }
+    
+    public void setSuperCharge(boolean superCharge)
+    {
+        this.superCharge=superCharge;
+    }
+    public void setSuperChargeRate(int superChargeRate)
+    {
+        this.superChargeRate=superChargeRate;
+    }
     
 }
--- src/Models/HybridVehicle.java
+++ src/Models/HybridVehicle.java
@@ -10,4 +10,5 @@ package Models;
  */
 public class HybridVehicle {
     
+    
 }
